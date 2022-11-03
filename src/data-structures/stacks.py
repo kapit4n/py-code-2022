6af40828532c81732fe1d4360d66ba6b673bd23d
Luis Arce <luis.arce22@gmail.com>
2022-11-03 14:17:36 -0400

    Add some python code

---
+++ src/data-structures/stacks.py
@@ -0,0 +1,11 @@
+stack = [1, 2, 3]
+print(stack, "stack")
+
+stack.append(6)
+print(stack, "append 6")
+
+stack.append(7)
+print(stack, "append 7")
+
+stack.pop()
+print(stack, "pop")
---
+++ structure-f.md
@@ -0,0 +1,8 @@
+# Features
+## Data structures
+### Stacks
+- popleft, append
+```
+stack = [1, 2, 3]
+```
+### List comprehensions
